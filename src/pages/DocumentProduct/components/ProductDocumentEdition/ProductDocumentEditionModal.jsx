aa40203bcf655d6adc413f6f694b747c9f1eca09
parisasarraf <parisasarraf79@gmail.com>
2025-06-26 22:30:07 +0330

    feat(docs): prevent duplicate document editions

--- src/pages/DocumentProduct/components/ProductDocumentEdition/ProductDocumentEditionModal.jsx
+++ src/pages/DocumentProduct/components/ProductDocumentEdition/ProductDocumentEditionModal.jsx
@@ -1,79 +1,74 @@
-import { useEffect } from "react";
-import { Col, Form, message, Row, Select } from "antd";
+import {useEffect} from "react";
+import {Col, Form, message, Row, Select} from "antd";
 import moment from "moment/moment";
 import Modal from "../../../../components/Modal";
-import { useCreateProductDocumentEdition, useUpdateProductDocumentEdition } from "../../../../QueryServises/productDocumentQuery";
+import {
+    useCreateProductDocumentEdition,
+    useUpdateProductDocumentEdition,
+    useProductDocumentTreeById
+} from "@/QueryServises/productDocumentQuery/index.js";
 import DatepickerCustom from "../../../../components/DatePicker";
-import { useProductById } from "../../../../QueryServises/productQuery";
 import FileUploader from "../../../../components/FileUploader/FileUploader";
-import { BASEURL } from "../../../../Services/axiosInstance";
+import {BASEURL} from "@/Services/axiosInstance.js";
 
-const ProductDocumentEditionModal = ({ isOpen, modalMode, modalData, closeModal, refetch }) => {
+const ProductDocumentEditionModal = ({isOpen, modalMode, modalData, closeModal, refetch, currentProduct}) => {
     const [form] = Form.useForm();
-    const { isPending: isCreating, mutateAsync: createProductDocumentEdition } = useCreateProductDocumentEdition();
-    const { isPending: isUpdating, mutateAsync: updateProductDocumentEdition } = useUpdateProductDocumentEdition();
+    const {isPending: isCreating, mutateAsync: createProductDocumentEdition} = useCreateProductDocumentEdition();
+    const {isPending: isUpdating, mutateAsync: updateProductDocumentEdition} = useUpdateProductDocumentEdition();
+
+    const {refetch: refetchDocumentTree} = useProductDocumentTreeById(currentProduct?.id, {
+        enabled: false
+    });
+
+
+    const findDocumentNodeById = (nodes, id) => {
+        for (const node of nodes) {
+            if (node.id === id) {
+                return node;
+            }
+            if (node.children) {
+                const found = findDocumentNodeById(node.children, id);
+                if (found) {
+                    return found;
+                }
+            }
+        }
+        return null;
+    };
+
 
     useEffect(() => {
         if (modalMode === "edition") {
-            form.resetFields()
+            form.resetFields();
         } else {
             form.setFieldsValue({
                 edition: modalData?.edition,
                 survey_date: modalData?.survey_date ? moment(modalData.survey_date) : null,
-                file_1: modalData?.file_1
-                    ? [
-                        {
-                            uid: "-1",
-                            name: "file_1",
-                            url: BASEURL.replace("/api/v1", "") + modalData.file_1,
-                        },
-                    ]
-                    : [],
-                file_2: modalData?.file_2
-                    ? [
-                        {
-                            uid: "-2",
-                            name: "file_2",
-                            url: BASEURL.replace("/api/v1", "") + modalData.file_2,
-                        },
-                    ]
-                    : [],
-                file_3: modalData?.file_3
-                    ? [
-                        {
-                            uid: "-3",
-                            name: "file_3",
-                            url: BASEURL.replace("/api/v1", "") + modalData.file_3,
-                        },
-                    ]
-                    : [],
-                file_4: modalData?.file_4
-                    ? [
-                        {
-                            uid: "-4",
-                            name: "file_4",
-                            url: BASEURL.replace("/api/v1", "") + modalData.file_4,
-                        },
-                    ]
-                    : [],
+                file_1: modalData?.file_1 ? [{
+                    uid: "-1",
+                    name: "file_1",
+                    url: BASEURL.replace("/api/v1", "") + modalData.file_1
+                }] : [],
+                file_2: modalData?.file_2 ? [{
+                    uid: "-2",
+                    name: "file_2",
+                    url: BASEURL.replace("/api/v1", "") + modalData.file_2
+                }] : [],
+                file_3: modalData?.file_3 ? [{
+                    uid: "-3",
+                    name: "file_3",
+                    url: BASEURL.replace("/api/v1", "") + modalData.file_3
+                }] : [],
+                file_4: modalData?.file_4 ? [{
+                    uid: "-4",
+                    name: "file_4",
+                    url: BASEURL.replace("/api/v1", "") + modalData.file_4
+                }] : [],
             });
         }
     }, [modalMode, modalData, form]);
 
     const onFinishForm = async (values) => {
-        const { data: productDocument } = await refetch();
-        const existingEditions = productDocument?.product_documents
-            ?.find(doc => doc.id === modalData?.id)
-            ?.editions || [];
-
-        const isEditionExist = existingEditions.some(
-            edition => edition.edition === values.edition
-        );
-
-        if (isEditionExist && modalMode === "edition") {
-            message.error("این نسخه قبلاً اضافه شده است");
-            return;
-        }
         const payload = {
             product_document_id: modalData?.product_document_id?.id,
             edition: values.edition,
@@ -85,7 +80,9 @@ const ProductDocumentEditionModal = ({ isOpen, modalMode, modalData, closeModal,
             file_2: values.file_2?.[0]?.originFileObj,
             file_3: values.file_3?.[0]?.originFileObj,
             file_4: values.file_4?.[0]?.originFileObj,
+
         };
+
 
         try {
             if (modalMode === "edition") {
@@ -94,18 +91,16 @@ const ProductDocumentEditionModal = ({ isOpen, modalMode, modalData, closeModal,
             } else {
                 await updateProductDocumentEdition({
                     documentId: modalData?.id,
-                    ...payload
+                    ...payload,
+                    product_document_id: modalData.product_document_id,
                 });
                 message.success("نسخه با موفقیت ویرایش شد");
             }
             refetch();
             closeModal();
         } catch (error) {
-            if (error.response?.data?.detail === "No ProductDocument matches the given query.") {
-                message.error("در حال حاضر این سند وجود ندارد");
-            } else {
-                message.error("موفقیت آمیز نبود، دوباره امتحان کنید");
-            }
+            const errorMessage = error.response?.data?.detail || "عملیات موفقیت آمیز نبود، دوباره امتحان کنید";
+            message.error(errorMessage);
             console.error("Error details:", error.response?.data);
         }
     };
@@ -120,37 +115,36 @@ const ProductDocumentEditionModal = ({ isOpen, modalMode, modalData, closeModal,
             mode={modalMode}
             loading={isCreating || isUpdating}
         >
-            <Form
-                form={form}
-                layout="vertical"
-                onFinish={onFinishForm}
-            >
+            <Form form={form} layout="vertical" onFinish={onFinishForm}>
                 <Row gutter={[16, 16]}>
                     <Col span={12}>
                         <Form.Item
                             label="نام نسخه"
                             name="edition"
                             rules={[
-                                { required: true, message: "لطفا نام نسخه را انتخاب کنید" },
+                                {required: true, message: "لطفا نام نسخه را انتخاب کنید"},
                                 {
                                     validator: async (_, value) => {
-                                        if (modalMode === "edition" && value) {
-                                            const { data: productDocument } = await refetch();
-                                            const existingEditions = productDocument?.product_documents
-                                                ?.find(doc => doc.id === modalData?.id)
-                                                ?.editions || [];
-
-                                            if (existingEditions.some(edition => edition.edition === value)) {
+                                        if (modalMode !== "edition" || !value) {
+                                            return Promise.resolve();
+                                        }
+                                        const {data: productDocument} = await refetchDocumentTree();
+                                        const documentNode = findDocumentNodeById(productDocument || [], modalData.id);
+                                        if (documentNode && documentNode.edition) {
+                                            const isEditionExist = documentNode.edition.some(
+                                                (ed) => ed.edition.toUpperCase() === value.toUpperCase()
+                                            );
+                                            if (isEditionExist) {
                                                 return Promise.reject(new Error('این نسخه قبلاً اضافه شده است'));
                                             }
                                         }
                                         return Promise.resolve();
-                                    }
-                                }
+                                    },
+                                },
                             ]}
                         >
                             <Select
-                                options={Array.from({ length: 26 }, (_, i) => ({
+                                options={Array.from({length: 26}, (_, i) => ({
                                     value: String.fromCharCode(97 + i).toUpperCase(),
                                     label: String.fromCharCode(97 + i).toUpperCase(),
                                 }))}
@@ -160,43 +154,28 @@ const ProductDocumentEditionModal = ({ isOpen, modalMode, modalData, closeModal,
                         </Form.Item>
                     </Col>
                     <Col span={12}>
-                        <Form.Item
-                            label="تاریخ بررسی"
-                            name="survey_date"
-                        >
-                            <DatepickerCustom format="YYYY-MM-DD" />
+                        <Form.Item label="تاریخ بررسی" name="survey_date">
+                            <DatepickerCustom format="YYYY-MM-DD"/>
                         </Form.Item>
                     </Col>
                     <Col span={6}>
-                        <Form.Item
-                            label="فایل 1"
-                            name="file_1"
-                        >
-                            <FileUploader maxCount={1} />
+                        <Form.Item label="فایل 1" name="file_1">
+                            <FileUploader maxCount={1}/>
                         </Form.Item>
                     </Col>
                     <Col span={6}>
-                        <Form.Item
-                            label="فایل 2"
-                            name="file_2"
-                        >
-                            <FileUploader maxCount={1} />
+                        <Form.Item label="فایل 2" name="file_2">
+                            <FileUploader maxCount={1}/>
                         </Form.Item>
                     </Col>
                     <Col span={6}>
-                        <Form.Item
-                            label="فایل 3"
-                            name="file_3"
-                        >
-                            <FileUploader maxCount={1} />
+                        <Form.Item label="فایل 3" name="file_3">
+                            <FileUploader maxCount={1}/>
                         </Form.Item>
                     </Col>
                     <Col span={6}>
-                        <Form.Item
-                            label="فایل 4"
-                            name="file_4"
-                        >
-                            <FileUploader maxCount={1} />
+                        <Form.Item label="فایل 4" name="file_4">
+                            <FileUploader maxCount={1}/>
                         </Form.Item>
                     </Col>
                 </Row>
