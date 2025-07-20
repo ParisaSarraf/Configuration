6f871e8512977590fc27af00e107f9c1f83801d0
ParisaSarraf <Parisasarraf79@gmail.com>
2025-07-20 13:02:38 +0330

    feat : handle children table and add product name above of pic and description

--- src/pages/Introduction/Introduction.jsx
+++ src/pages/Introduction/Introduction.jsx
@@ -1,8 +1,8 @@
 import { useProductContext } from '../../Services/Context/ProductContext';
-import { Card, Typography, Row, Col, Space, Image, Skeleton, Table, Input } from 'antd';
+import { Card, Typography, Row, Col, Space, Image, Skeleton, Table, Input, ConfigProvider } from 'antd';
+import fa_IR from 'antd/locale/fa_IR';
 import { BASEURL } from "@/Services/axiosInstance.js";
 import ProductCols from './components/ProductCols';
-import TextArea from 'antd/es/input/TextArea';
 
 const { Title, Text } = Typography;
 
@@ -12,21 +12,16 @@ const RecursiveTable = ({ dataSource, columns }) => {
             columns={columns}
             dataSource={dataSource}
             rowKey="id"
-            expandable={{
-                expandedRowRender: (record) => (
-                    record.children && record.children.length > 0 ? (
-                        <RecursiveTable
-                            dataSource={record.children}
-                            columns={columns}
-                        />
-                    ) : null
-                ),
-                rowExpandable: (record) => record.children && record.children.length > 0,
-            }}
             pagination={false}
+            expandable={{
+                indentSize: 20,
+                expandIconColumnIndex: 0,
+                rowExpandable: (record) => record.children && record.children.length > 0
+            }}
         />
     );
 };
+
 
 const Introduction = () => {
     const { currentProduct } = useProductContext();
@@ -37,62 +32,68 @@ const Introduction = () => {
     }
 
     return (
-        <div style={{ padding: 16 }}>
-            <Card className="mb-1">
-                <Row gutter={16} align="middle">
-                    <Col span={12}>
-                        <Space direction="vertical" className='w-full'>
-                            <Input.TextArea
-                                rows={5}
-                                size='small'
-                                placeholder='توضیحات محصول'
-                                style={{
+        <ConfigProvider direction="rtl" locale={fa_IR}>
+            <div style={{ padding: 16 }}>
+                <Card className="mb-1">
+                    <Row gutter={16} align="middle">
+                        <Col span={24}>
+                            <Title level={4}>{product?.persian_title}</Title>
+                        </Col>
+                        <Col span={12}>
+                            <Space direction="vertical" className='w-full'>
+                                <Input.TextArea
+                                    rows={5}
+                                    size='small'
+                                    placeholder='توضیحات محصول'
+                                    style={{
+                                        height: 350,
+                                        width: '100%',
+                                        resize: 'none'
+                                    }}
+                                />
+                            </Space>
+                        </Col>
+                        <Col span={12}>
+                            {product.image ? (
+                                <Image
+                                    width="100%"
+                                    style={{
+                                        maxHeight: 400,
+                                        height: 350,
+                                        objectFit: 'contain',
+                                        width: '100%',
+                                        justifyContent: 'center',
+                                        backgroundColor: '#E2E2E2'
+                                    }}
+                                    src={`${BASEURL.replace("/api/v1", "")}${product.image}`}
+                                    alt="تصویر محصول"
+                                />
+                            ) : (
+                                <div style={{
                                     height: 350,
                                     width: '100%',
-                                    resize: 'none'
-                                }}
-                            />
-                        </Space>
-                    </Col>
-                    <Col span={12}>
-                        {product.image ? (
-                            <Image
-                                width="100%"
-                                style={{
-                                    maxHeight: 400,
-                                    height: 350,
-                                    objectFit: 'contain',
-                                    width: '100%',
+                                    display: 'flex',
+                                    alignItems: 'center',
                                     justifyContent: 'center',
                                     backgroundColor: '#E2E2E2'
-                                }}
-                                src={`${BASEURL.replace("/api/v1", "")}${product.image}`}
-                                alt="تصویر محصول"
-                            />
-                        ) : (
-                            <div style={{
-                                height: 350,
-                                width: '100%',
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: '#E2E2E2'
-                            }}>
-                                <Skeleton.Image />
-                            </div>
-                        )}
-                    </Col>
-                </Row>
-            </Card>
-            {product.children && product.children.length > 0 && (
-                <Card title="محصولات زیرمجموعه" >
-                    <RecursiveTable
-                        dataSource={product.children}
-                        columns={ProductCols()}
-                    />
+                                }}>
+                                    <Skeleton.Image />
+                                </div>
+                            )}
+                        </Col>
+                    </Row>
                 </Card>
-            )}
-        </div>
+
+                {product.children && product.children.length > 0 && (
+                    <Card title="محصولات زیرمجموعه" >
+                        <RecursiveTable
+                            dataSource={product.children}
+                            columns={ProductCols()}
+                        />
+                    </Card>
+                )}
+            </div>
+        </ConfigProvider>
     );
 };
 
