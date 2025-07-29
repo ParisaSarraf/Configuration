67ec96bc9b40ca7b3800e93e2e8431422649b650
ParisaSarraf <Parisasarraf79@gmail.com>
2025-07-29 12:49:09 +0330

    feat : picture size fixed

--- src/pages/Introduction/Introduction.jsx
+++ src/pages/Introduction/Introduction.jsx
@@ -90,7 +90,7 @@ const Introduction = () => {
     return (
         <ConfigProvider direction="rtl" locale={fa_IR}>
             <div style={{ padding: 16 }}>
-                <Card className="mb-1">
+                <Card className="-mb-8">
                     <Row gutter={16}>
                         <Col span={24}>
                             <Title level={4}>{productData?.persian_title}</Title>
@@ -103,7 +103,7 @@ const Introduction = () => {
                                 className="w-full"
                             >
                                 <Row gutter={16}>
-                                    <Col span={12}>
+                                    <Col span={8}>
                                         <Form.Item
                                             name="user_description"
                                         >
@@ -124,29 +124,30 @@ const Introduction = () => {
                                             <FileUploader maxFiles={1} listType="picture" />
                                         </Form.Item>
 
-                                        <Form.Item>
-                                            <Button type="primary" htmlType="submit">
-                                                ذخیره تغییرات
-                                            </Button>
-                                        </Form.Item>
                                     </Col>
 
-                                    <Col span={12}>
+                                    <Col span={16} className='flex flex-col space-y-16 '>
                                         {productData.user_image && (
                                             <Image
                                                 src={`${BASEURL.replace("/api/v1", "")}${productData.user_image}`}
                                                 alt="تصویر محصول"
                                                 style={{
                                                     height: 350,
-                                                    width: '100%',
+                                                    width: 930,
                                                     objectFit: 'contain',
                                                     backgroundColor: '#E2E2E2',
                                                     borderRadius: 4,
                                                 }}
                                             />
                                         )}
+                                        <Form.Item className='flex justify-end'>
+                                            <Button type="primary" htmlType="submit">
+                                                ذخیره تغییرات
+                                            </Button>
+                                        </Form.Item>
                                     </Col>
                                 </Row>
+
                             </Form>
                         </Col>
                     </Row>
