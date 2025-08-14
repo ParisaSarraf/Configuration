c83592180941665019593e8801df1561ac7418be
parisasarraf <parisasarraf79@gmail.com>
2025-08-14 17:31:30 +0330

    fix : some change for background color

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -78,11 +78,11 @@ const Activity = () => {
         const today = new Date();
         const dueDate = record.to_date ? new Date(record.to_date) : null;
         if (record.state === 10 && dueDate && dueDate < today) {
-            return 'bg-red-100';
+            return 'bg-red-100 text-red-800';
         } else if (record.state === 20) {
-            return 'bg-yellow-100';
+            return 'bg-yellow-100 text-yellow-800';
         } else if (record.state === 30) {
-            return 'bg-green-100';
+            return 'bg-green-100 text-green-800';
         }
         return '';
     };
