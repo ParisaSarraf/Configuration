128b2decd15ffcb14b8cb47955489c0f56a5fb96
Reza <reza_m_n_65@yahoo.com>
2025-08-20 01:23:58 +0330

    تغییر ستون های گزارش اسناد

--- src/pages/Reports/components/CollapseTableReport/ReportCols.jsx
+++ src/pages/Reports/components/CollapseTableReport/ReportCols.jsx
@@ -3,30 +3,12 @@ import {Tag} from "antd";
 const ReportCol = () => {
     return [
         {
-            title: 'نام نسخه',
-            dataIndex: ['editions', 0, 'state_name'],
-            key: 'state_name',
-            width: 100,
-            render: (record) => {
-                return (<Tag color={'red'}>{record}</Tag>)
-            }
-        },
-        {
             title: 'نام محصول',
             dataIndex: ['product', 'persian_title'],
             key: 'persian_title',
             width: 100,
             render: (record) => {
                 return (<Tag color={'cyan'}>{record}</Tag>)
-            }
-        },
-        {
-            title: 'کد محصول',
-            key: 'code',
-            dataIndex: ['product', 'code'],
-            width: 100,
-            render: (record) => {
-                return (<Tag color={'purple'}>{record}</Tag>)
             }
         },
         {
@@ -38,16 +20,33 @@ const ReportCol = () => {
                 return (<Tag color={'gold'}>{record}</Tag>)
             }
         },
+        // {
+        //     title: 'کد محصول',
+        //     key: 'code',
+        //     dataIndex: ['product', 'code'],
+        //     width: 100,
+        //     render: (record) => {
+        //         return (<Tag color={'purple'}>{record}</Tag>)
+        //     }
+        // },
         {
-            title: 'نام نسخه',
+            title: 'کد سند',
             dataIndex: ['editions', 0, 'edition'],
             key: 'edition',
             width: 100,
             render: (record) => {
                 return (<Tag color={'blue'}>{record}</Tag>)
             }
-        }
-
+        },
+        {
+            title: 'وضعیت سند',
+            dataIndex: ['editions', 0, 'state_name'],
+            key: 'state_name',
+            width: 100,
+            render: (record) => {
+                return (<Tag color={'red'}>{record}</Tag>)
+            }
+        },
     ]
 }
 
