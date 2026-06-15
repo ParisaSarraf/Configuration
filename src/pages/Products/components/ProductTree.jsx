e369a108e973d9a53b697c505652487dcb046c7f
ParisaSarraf <parisasarraf79@gmail.com>
2026-06-15 16:05:28 +0330

    style(ProductTree): remove unused useRef import

--- src/pages/Products/components/ProductTree.jsx
+++ src/pages/Products/components/ProductTree.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { message, Modal, Progress, Typography } from "antd";
 import {
   DeleteOutlined,
