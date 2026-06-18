81caeb6cf8e60d90528aabc65ff19ed01be16c52
parisa <Parisasarraf79@gmail.com>
2026-06-18 09:59:39 +0330

    fix(RequestOfWarehouseCol): add initialValue to Form.Item for confirmed number

--- src/pages/productPurchase/components/RequestOfWarehouse/RequestOfWarehouseCol.jsx
+++ src/pages/productPurchase/components/RequestOfWarehouse/RequestOfWarehouseCol.jsx
@@ -1,43 +1,49 @@
-import {Form, Input} from "antd";
+import { Form, Input } from "antd";
 
 const RequestOfWarehouseCol = () => {
-    return [
-        {
-            title: 'عنوان محصول',
-            dataIndex: 'name',
-            key: 'name'
-        },
-        {
-            title: 'تعداد کل',
-            dataIndex: 'quantity',
-            key: 'quantity',
-            render: (text) => text || 'ندارد'
-        },
-        {
-            title: 'تعداد مورد تایید',
-            render: (_, record) => (
-                <Form.Item
-                    name={["confirmed_number", record.id]}
-                    className="mb-0"
-                    rules={[
-                        {
-                            validator: (_, value) => {
-                                if (value && value < 0) {
-                                    return Promise.reject('تعداد نمی‌تواند منفی باشد');
-                                }
-                                if (value && !/^\d+$/.test(value)) {
-                                    return Promise.reject('لطفا فقط عدد وارد کنید');
-                                }
-                                return Promise.resolve();
-                            }
-                        }
-                    ]}
-                >
-                    <Input type="number" min={0} placeholder="تعداد را وارد کنید"/>
-                </Form.Item>
-            )
-        },
-    ];
+  return [
+    {
+      title: "عنوان محصول",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "تعداد کل",
+      dataIndex: "quantity",
+      key: "quantity",
+      render: (text) => text || "ندارد",
+    },
+    {
+      title: "تعداد مورد تایید",
+      render: (_, record) => (
+        <Form.Item
+          name={["confirmed_number", record.id]}
+          initialValue={record.quantity}
+          className="mb-0"
+          rules={[
+            {
+              validator: (_, value) => {
+                if (value && value < 0) {
+                  return Promise.reject("تعداد نمی‌تواند منفی باشد");
+                }
+                if (!/^\d+(\.\d+)?$/.test(value)) {
+                  return Promise.reject("لطفا فقط عدد وارد کنید");
+                }
+                return Promise.resolve();
+              },
+            },
+          ]}
+        >
+          <Input
+            type="number"
+            min={0}
+            placeholder="تعداد را وارد کنید"
+            step="0.01"
+          />
+        </Form.Item>
+      ),
+    },
+  ];
 };
 
 export default RequestOfWarehouseCol;
